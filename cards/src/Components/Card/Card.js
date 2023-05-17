26722eba8a7e3f048182c4317bcbe9feff3c5e73
jonathon lee <jonnyleealas@gmail.com>
2023-05-17 11:53:08 -0500

    fixed div for components

--- cards/src/Components/Card/Card.js
+++ cards/src/Components/Card/Card.js
@@ -1,13 +1,15 @@
 import './card.css'
 const Card = (props) => {
     return (
+
         <div className='card'>
-            <div>Image</div>
             <p>{props.data.id}</p>
             <p>{props.data.title}</p>
             <p>{props.data.description}</p>
             <p>{props.data.googleMapsUrl}</p>
         </div>
+
+
     )
 }
 

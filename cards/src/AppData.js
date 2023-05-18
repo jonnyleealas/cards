e848b6976dc477150cbec7bf93148d4ab1cbb5fb
jonathon lee <jonnyleealas@gmail.com>
2023-05-17 23:16:27 -0500

    added image

--- cards/src/AppData.js
+++ cards/src/AppData.js
@@ -1,7 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
+import fuji from '../src/pics/fuji.jpeg'
+
+
 export default [
     {
      id: 1,
+     picture: fuji,
      title: "Japan",
      googleMapsUrl: "https.//goo.gl/maps/1DGM5WrWnATgkSNB8",
      startDate: "12 Jan, 2021",
@@ -9,67 +13,5 @@ export default [
      description: " Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters. Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
      imageUrl: "https://unsplash.com/photos/WLxQvbMyfas",   
     },
-    {
-        id: 1,
-        title: "Japan",
-        googleMapsUrl: "https.//goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan 2021",
-        description: " Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters. Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas",   
-       },
-    {
-        id: 1,
-        title: "Japan",
-        googleMapsUrl: "https.//goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan 2021",
-        description: " Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters. Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas",   
-       },
-    {
-        id: 1,
-        title: "Japan",
-        googleMapsUrl: "https.//goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan 2021",
-        description: " Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters. Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas",   
-       },
-    {
-        id: 1,
-        title: "Japan",
-        googleMapsUrl: "https.//goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan 2021",
-        description: " Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters. Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas",   
-       },
-    {
-        id: 1,
-        title: "Japan",
-        googleMapsUrl: "https.//goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan 2021",
-        description: " Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters. Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas",   
-       },
-    {
-        id: 1,
-        title: "Japan",
-        googleMapsUrl: "https.//goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan 2021",
-        description: " Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters. Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas",   
-       },
-    {
-        id: 1,
-        title: "Japan",
-        googleMapsUrl: "https.//goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan 2021",
-        description: " Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters. Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas",   
-       },
+
 ]
--- cards/src/Components/Card/Card.js
+++ cards/src/Components/Card/Card.js
@@ -3,10 +3,14 @@ const Card = (props) => {
     return (
 
         <div className='card'>
+            <img className='picture' src={props.data.picture} alt='fuji' />
+            <div className='card-content' >
             <p>{props.data.id}</p>
             <p>{props.data.title}</p>
             <p>{props.data.description}</p>
             <p>{props.data.googleMapsUrl}</p>
+            </div>
+            
         </div>
 
 

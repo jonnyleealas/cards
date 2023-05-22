8cdd306918a62f1bb4162bec97064198b7fb056f
jonathon lee <jonnyleealas@gmail.com>
2023-05-22 11:16:08 -0500

    added pin link logo
    Need to work on linking url using react link

--- cards/src/Components/Card/Card.js
+++ cards/src/Components/Card/Card.js
@@ -1,4 +1,5 @@
 import './card.css'
+import pin from '../pics/location-pin.png'
 const Card = (props) => {
     return (
 
@@ -6,10 +7,11 @@ const Card = (props) => {
 
             <img className='picture' src={props.data.picture} alt='fuji' />
             <div className='card-content' >
+                <img className='location-pin' src={pin} atl='location pin' />
+                <p>{props.data.googleMapsUrl}</p>
                 <p>{props.data.id}</p>
                 <p>{props.data.title}</p>
                 <p>{props.data.description}</p>
-                <p>{props.data.googleMapsUrl}</p>
             </div>
            
         </div>

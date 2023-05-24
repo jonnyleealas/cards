7fc11cb10394cceec9b8322816dfc83cfd630e26
jonathon lee <jonnyleealas@gmail.com>
2023-05-23 20:44:30 -0500

    fixed font in card

--- cards/src/Components/Card/Card.js
+++ cards/src/Components/Card/Card.js
@@ -11,9 +11,9 @@ const Card = (props) => {
             </div>
             <div className='card-content' >
                 <div className='pin-content'>
-                    <img className='location-pin' src={pin} atl='location pin' />
+                    <img className='location-pin-icon' src={pin} atl='location pin' />
                     <span className='country'>{props.data.country}</span>
-                    <a href={props.data.googleMapsUrl} target='_blank'>View on Google Maps</a>
+                    <a className='link' href={props.data.googleMapsUrl} target='_blank'>View on Google Maps</a>
                 </div>
                 <div className='title' >{props.data.title}</div>
                 <div className='start-date' >{props.data.startDate}</div>

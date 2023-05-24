37ca8021b4676e8284284f8728ca4dc2a815fe58
jonathon lee <jonnyleealas@gmail.com>
2023-05-24 11:37:29 -0500

    fixed link height

--- cards/src/Components/Card/Card.js
+++ cards/src/Components/Card/Card.js
@@ -10,11 +10,14 @@ const Card = (props) => {
                 <img className='picture' src={props.data.picture} alt='fuji' />
             </div>
             <div className='card-content' >
-                <div className='pin-content'>
+                <span className='pin-content'>
                     <img className='location-pin-icon' src={pin} atl='location pin' />
                     <span className='country'>{props.data.country}</span>
-                    <a className='link' href={props.data.googleMapsUrl} target='_blank'>View on Google Maps</a>
-                </div>
+                    <div className='link'>
+
+                    <a href={props.data.googleMapsUrl} target='_blank'>View on Google Maps</a>
+                    </div>
+                </span>
                 <div className='title' >{props.data.title}</div>
                 <div className='start-date' >{props.data.startDate}</div>
                 <div className='description' >{props.data.description}</div>

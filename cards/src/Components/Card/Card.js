36cf527c486c8ff2a0630345cde4c446b0770d20
jonathon lee <jonnyleealas@gmail.com>
2023-05-29 20:04:45 -0500

    cards done

--- cards/src/Components/Card/Card.js
+++ cards/src/Components/Card/Card.js
@@ -14,15 +14,13 @@ const Card = (props) => {
                     <img className='location-pin-icon' src={pin} atl='location pin' />
                     <span className='country'>{props.data.country}</span>
                     <div className='link'>
-
-                    <a href={props.data.googleMapsUrl} target='_blank'>View on Google Maps</a>
+                        <a href={props.data.googleMapsUrl} target='_blank'>View on Google Maps</a>
                     </div>
                 </span>
                 <div className='title' >{props.data.title}</div>
                 <div className='start-date' >{props.data.startDate}</div>
                 <div className='description' >{props.data.description}</div>
             </div>
-
         </div>
 
 
